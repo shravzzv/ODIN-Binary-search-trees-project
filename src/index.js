ef7c4a1a02db2eaf9b79d89e52f467f75d343e13
sai shravan <saishravan384@gmail.com>
2023-11-11 10:25:43 +0530

    refactor: Update index for improved logs readablility

--- src/index.js
+++ src/index.js
@@ -26,32 +26,41 @@ function getRandomIntInclusive(min, max) {
 }
 
 const randomNums = new Set()
-for (let i = 0; [...randomNums].length < 50; i++) {
+for (let i = 0; [...randomNums].length < 10; i++) {
   randomNums.add(getRandomIntInclusive(0, 99))
 }
 
 const tree = new Tree([...randomNums])
 
-console.log(tree.isBalanced())
+console.log('A binary search tree created using 10 random integers <100:\n')
+tree.prettyPrint()
 
-console.log(tree.levelOrder())
-console.log(tree.preOrder())
-console.log(tree.postOrder())
-console.log(tree.inOrder())
+console.log('Is Balanced? ' + tree.isBalanced())
 
-for (let i = 0; i < 50; i++) {
+console.log('Level order: ' + tree.levelOrder())
+console.log('Pre order: ' + tree.preOrder())
+console.log('Post order: ' + tree.postOrder())
+console.log('In order: ' + tree.inOrder())
+
+for (let i = 0; i < 15; i++) {
   tree.insert(getRandomIntInclusive(101, 200))
 }
 
-console.log(tree.isBalanced())
+console.log(
+  '\nInserting into the binary serach tree numbers > 100 to unbalance it:\n'
+)
+tree.prettyPrint()
+
+console.log('\nIs Balanced? ' + tree.isBalanced())
 
 tree.rebalance()
 
-console.log(tree.isBalanced())
-
-console.log(tree.levelOrder())
-console.log(tree.preOrder())
-console.log(tree.postOrder())
-console.log(tree.inOrder())
-
+console.log('\nAfter balancing the tree:\n')
 tree.prettyPrint()
+
+console.log('\nIs Balanced? ' + tree.isBalanced())
+
+console.log('\nLevel order: ' + tree.levelOrder())
+console.log('\nPre order: ' + tree.preOrder())
+console.log('\nPost order: ' + tree.postOrder())
+console.log('\nIn order: ' + tree.inOrder())
